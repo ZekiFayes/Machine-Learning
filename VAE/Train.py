f07c5a2d60365377394a66c0e0c2c0b6a555dce5
Jingren XIE <33126026+ZekiFayes@users.noreply.github.com>
2019-07-08 12:19:30 +0800

    Add files via upload

--- VAE/Train.py
+++ VAE/Train.py
@@ -14,7 +14,7 @@ def train(x_inputs):
     with tf.Session() as sess:
         sess.run(init)
 
-        # saver.restore(sess, model_path)
+        saver.restore(sess, model_path)
 
         for i in range(1, param_vae['num_steps'] + 1):
             batch_x, _ = x_inputs.train.next_batch(param_vae['batch_size'])
@@ -24,21 +24,3 @@ def train(x_inputs):
                 print('Step %i, Loss: %f' % (i, loss))
 
         saver.save(sess, model_path)
-
-        noise_input = tf.placeholder(tf.float32, shape=[None, param_vae['latent']])
-        decoder = vae_decoder(noise_input)
-        n = 20
-        x_axis = np.linspace(-3, 3, n)
-        y_axis = np.linspace(-3, 3, n)
-
-        canvas = np.empty((28 * n, 28 * n))
-        for i, yi in enumerate(x_axis):
-            for j, xi in enumerate(y_axis):
-                z_mu = np.array([[xi, yi]] * param_vae['batch_size'])
-                x_mean = sess.run(decoder, feed_dict={noise_input: z_mu})
-                canvas[(n - i - 1) * 28:(n - i) * 28, j * 28:(j + 1) * 28] = x_mean[0].reshape(28, 28)
-
-        plt.figure(figsize=(8, 10))
-        Xi, Yi = np.meshgrid(x_axis, y_axis)
-        plt.imshow(canvas, origin="upper", cmap="gray")
-        plt.show()
