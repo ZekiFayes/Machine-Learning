35aacf595460402ac5f3614e398410bb1af4472e
Jingren XIE <33126026+ZekiFayes@users.noreply.github.com>
2019-07-12 13:10:36 +0800

    Add files via upload

--- Lib.py
+++ Lib.py
@@ -1,20 +1,26 @@
-import pandas as pd
-from pandas import DataFrame, Series
+import tensorflow as tf
 import numpy as np
-from sklearn import datasets
+import pickle
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
-from matplotlib.colors import ListedColormap
-from Data_preprocessing import load_split_standardize_data
-from Plot import plot
-from Classifier import optimize_classifier, train
-from Optimization import optimization_and_recommendation
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
 
+param_rbm = {
+    'learning_rate': 0.1,
+    'batch_size': 1,
+    'num_epochs': 3,
+    'num_v_nodes': 3952*5,
+    'num_h_nodes': 200,
+    'initial_mu': 0.5,
+    'final_mu': 0.9,
+    'weight_cost': 0.0002,
+    'cd_k': 10,
+}
+
+wb = {
+    'w': tf.random_normal([param_rbm['num_v_nodes'], param_rbm['num_h_nodes']]),
+    'v': tf.random_normal([param_rbm['num_v_nodes']]),
+    'h': tf.random_normal([param_rbm['num_h_nodes']]),
+
+    'w_inc': tf.zeros([param_rbm['num_v_nodes'], param_rbm['num_h_nodes']]),
+    'v_inc': tf.zeros([param_rbm['num_v_nodes']]),
+    'h_inc': tf.zeros([param_rbm['num_h_nodes']]),
+}
